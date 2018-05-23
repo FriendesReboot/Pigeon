248ec79a4bc0a2d6630ad94bd0627de1e327ff7b
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-23 06:50:38 -0500

    Update Brain.js

--- Brain.js
+++ Brain.js
@@ -2,6 +2,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
+const prefix = "Pigeon, ";
 
 // This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
@@ -16,10 +17,10 @@ fs.readdir("./events/", (err, files) => {
 
 client.on("message", message => {
   if (message.author.bot) return;
-  if(message.content.indexOf("Pigeon, ") !== 0) return;
+  if(message.content.indexOf(prefix) !== 0) return;
 
   // This is the best way to define args. Trust me.
-  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
   // The list of if/else is replaced with those simple 2 lines:
