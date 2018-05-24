493120db887726ca697d6b184a56eeb6d74d32e9
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-23 19:34:07 -0500

    Update and rename animatedtest.js to skenner.js

--- commands/animatedtest.js
+++
@@ -1,4 +0,0 @@
-
-exports.run = (client, message, args) => {
-  message.channel.send("<a:spFnner:449003411388628995> Loading an Animated Emoji Test")
-}
---
+++ commands/skenner.js
@@ -0,0 +1,4 @@
+
+exports.run = (client, message, args) => {
+  message.channel.send("<a:spFnner:449003411388628995> Loading Icon 1 <a:SpFneer:449006726092554240> Loading Icon 2 Loading an Animated Emoji Test")
+}
