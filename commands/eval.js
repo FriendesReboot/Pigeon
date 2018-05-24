1036d2ebf81828a60d93afb3eb49bd0ef5b6ce95
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-24 07:09:14 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -16,5 +16,7 @@ exports.run = (client, message, args) => {
       message.channel.send(clean(evaled), {code:"xl"});
     } catch (err) {
       message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    } else {
+      message.reply(`\`ERROR\` \`\`\`xl\nNice try, but no.\n\`\`\``)
     }
 }
