d9e2a8be3a7c074df1fb2c0361c093d08349c6d1
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-25 07:17:48 -0500

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -1,1 +1,14 @@
-
+  function clean(text) {
+    if (typeof(text) === "string")
+    return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+      else
+    return text;
+};
+exports.run = (client, message, args) => {
+      const item = args.join(" ");
+      if item == "me a beach ball" {
+        message.reply("Here you go! <a:BouncingBeachBall:449383018793271307>");
+      } else {
+        message.reply("I cannot give that to you, sorry :(");
+      }
+}
