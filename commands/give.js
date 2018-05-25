7ffb9d57aac4d8e8daf0aa6fbbbba7dd638c7850
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-25 07:20:47 -0500

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -5,8 +5,8 @@
     return text;
 };
 exports.run = (client, message, args) => {
-      var item = args.join(" ");
-      if item === "me a beach ball" {
+      var itemgivename = args.join(" ");
+      if itemgivename === "me a beach ball" {
         message.reply("Here you go! <a:BouncingBeachBall:449383018793271307>");
       } else {
         message.reply("I cannot give that to you, sorry :(");
