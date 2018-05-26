655736fbf1bc92057996ccaf3e9c25d93b0fbb8f
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-26 07:39:42 -0500

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -10,6 +10,10 @@ exports.run = (client, message, args) => {
         message.reply("Here you go! <a:BouncingBeachBall:449383018793271307>");
       } else if (argumentals === "me a mac wait cursor") {
         message.reply("Spinning beach ball of doom... Well, can't stop ya. <a:BeachBall:449382607185117184>");
+      } else if (argumentals === "me a Finobe") {
+        message.reply("Here's your Finobe! https://finobe.com");
+      } else if (argumentals === "me a finobe") {
+            message.reply("Wrong spelling?");
       } else {
         message.reply("I cannot give that to you, sorry :(");
       }
