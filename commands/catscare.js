dfb31070d7554d503e2dda926806de7a49a38669
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-27 09:22:32 -0500

    Update catscare.js

--- commands/catscare.js
+++ commands/catscare.js
@@ -1,3 +1,11 @@
 exports.run = (Discord, client, message, args) => {
-
+   const embed = new  Discord.RichEmbed()
+        .setTitle("CatScare")
+        .setDescription("Cat, GET 'EM!")
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+        .setColor([135, 21, 153])
+        .setImage("https://giphy.com/gifs/LEYBoPcaAbDlC")
+        .setTimestamp();
+        message.reply({ embed } );
 }
