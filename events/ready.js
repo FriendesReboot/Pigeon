93a6570fc48160c53f34780c4e247994847dc4e6
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-27 08:43:35 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 exports.run = (client) => {
   console.log(`Ready to server in ${client.channels.size} channels on ${client.guilds.size} servers, for a total of ${client.users.size} users.`);
-  client.user.setPresence({ game: { name: 'ppu!(command) | BETA', type: 2 }, status: "dnd" });
+  client.user.setPresence({ game: { name: ' V61020 type some code...', type: 2 }, status: "dnd" });
 }
