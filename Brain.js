f82d1dfcef711938948e2360c60894d1e19adf3e
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-27 08:44:22 -0500

    Update Brain.js

--- Brain.js
+++ Brain.js
@@ -2,7 +2,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const fs = require("fs");
-const prefix = "ppu!";
+const prefix = "pro pigeon, ";
 
 // This loop reads the /events/ folder and attaches each event file to the appropriate event.
 fs.readdir("./events/", (err, files) => {
