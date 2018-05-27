93b82d55bd06d184801884228505f66777bd3dec
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-27 07:03:19 -0500

    Update Brain.js

--- Brain.js
+++ Brain.js
@@ -16,7 +16,7 @@ fs.readdir("./events/", (err, files) => {
 });
 
 client.on("message", message => {
-  const lowercasemsg = message.toLowerCase();
+  const lowercasemsg = message.toString().toLowerCase();
   if (message.author.bot) return;
   if(lowercasemsg.content.indexOf(prefix) !== 0) return;
 
