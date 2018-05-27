5898d31f8177c8f33198154f81c3ede2bc6d5e56
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-27 09:33:02 -0500

    Create about.js

---
+++ commands/about.js
@@ -0,0 +1,19 @@
+exports.run = (Discord, client, message, args) => {
+   const HelpEmbed = new  Discord.RichEmbed()
+        .setTitle("About Pigeon Professional")
+        .setDescription("Information on the bot. This command is exclusive to Pigeon Professional.")
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+        .setColor([135, 21, 153])
+        .setThumbnail("https://cdn.discordapp.com/avatars/450288720151707658/81a8e401f8e4f58292de9de35f85bf18.png?size=256")
+        .addField("Build", "1002", true)
+        .addField("Version", "Professional Unstable", true)
+        .addField("Server Count", bot.guilds.size.toString(), true)
+        .addField("Servers Inside", lient.guilds.map(g => g.name).join("\n"), true)
+        .addField("How to Get", "Invite Only", true)
+        .addBlankField(true)
+        .addField("Owner", "ConJoined#2400", true)
+        .addField("Contributors", "muhammed51#5559, ConJoined#2400", true)
+        .setTimestamp();
+   message.reply({ HelpEmbed })
+}
