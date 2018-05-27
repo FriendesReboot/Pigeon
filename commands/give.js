f7295ee1d5c9d94b8da3526ce9edbc01a425b2b8
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-27 08:03:56 -0500

    Update give.js

--- commands/give.js
+++ commands/give.js
@@ -1,4 +1,10 @@
-  function clean(text) {
+  var now = (function () {
+    var year = new Date(new Date().getFullYear().toString()).getTime();
+    return function () {
+        return Date.now() - year
+    }
+})();
+function clean(text) {
     if (typeof(text) === "string")
     return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
       else
@@ -15,6 +21,40 @@ exports.run = (client, message, args) => {
         message.reply("Here's your Finobe! https://finobe.com");
       } else if (lowerargumentals === "me finobe") {
         message.reply("Here's your Finobe! https://finobe.com");
+      } else if (lowerargumentals === "me help") {
+        message.reply("Oh no! Some help is on it's way! 🚑");
+        const HelpEmbed = new  Discord.RichEmbed()
+        .setTitle("Your Help 🚑")
+        .setDescription("Hey! I've got your help! To request me, say Pigeon, in any spelling!")
+        .setURL("https://discordapp.com")
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.username + "#" + message.author.tag, message.author.displayAvatarURL)
+        .setColor(316268)
+        .setThumbnail("https://cdn.discordapp.com/avatars/442358815690588200/0a89ae8f11cbaac2a3223b9531ef9ec9.png?size=256")
+        .addField("Fun", "Pigeon, give me [blank]\nDescription:\nGives you stuff\nThings to request:\nFinobe, Beach Ball, \nMac Wait Cursor,\nHelp\n ", true)
+        .addField("Informational", "Pigeon, give me help \nAlias: Pigeon, give me some help\nDescription:\nGives you help.\n ", true)
+        .addBlankField(true)
+        .addField("NSFW", "To see NSFW commands, type Pigeon, nsfw help in a NSFW channel")
+        .setTimestamp();
+        message.author.send("Transporting help to your place...")
+        message.author.send({embed: HelpEmbed})
+      } else if (lowerargumentals === "me some help") 
+        message.reply("Oh no! Some help is on it's way! 🚑");
+        const HelpEmbed = new  Discord.RichEmbed()
+        .setTitle("Your Help 🚑")
+        .setDescription("Hey! I've got your help! To request me, say Pigeon, in any spelling!")
+        .setURL("https://discordapp.com")
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.username + "#" + message.author.tag, message.author.displayAvatarURL)
+        .setColor(316268)
+        .setThumbnail("https://cdn.discordapp.com/avatars/442358815690588200/0a89ae8f11cbaac2a3223b9531ef9ec9.png?size=256")
+        .addField("Fun", "Pigeon, give me [blank]\nDescription:\nGives you stuff\nThings to request:\nFinobe, Beach Ball, \nMac Wait Cursor,\nHelp\n ", true)
+        .addField("Informational", "Pigeon, give me help \nAlias: Pigeon, give me some help\nDescription:\nGives you help.\n ", true)
+        .addBlankField(true)
+        .addField("NSFW", "To see NSFW commands, type Pigeon, nsfw help in a NSFW channel")
+        .setTimestamp();
+        message.author.send("Transporting help to your place...")
+        message.author.send({embed: HelpEmbed})
       } else {
         message.reply("I cannot give that to you, sorry :(");
       }
