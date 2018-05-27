deabb7f68def3ccb285966eb664f7fa98cfc83f7
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-27 08:09:15 -0500

    Update skenner.js

--- commands/skenner.js
+++ commands/skenner.js
@@ -1,4 +1,4 @@
 
-exports.run = (client, message, args) => {
+exports.run = (Discord, client, message, args) => {
   message.channel.send("<a:spFnner:449008770098528267> Loading Icon 1 <a:SpFneer:449008768978518036> Loading Icon 2 Loading an Animated Emoji Test")
 }
