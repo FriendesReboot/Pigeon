6592ef4e413610d466960141d14870f475df9770
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-28 14:12:12 -0500

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -1,7 +1,27 @@
-
-var guildList = client.guilds.array();
+exports.run = (Discord, client, message, args) => {
+        if(message.author.id !== "424367587934339084" && message.author.id !== "388323805124362240") {
+      const embed = new  Discord.RichEmbed()
+        .setTitle("You shall NOT pass!")
+        .setDescription("You are not authorized to use this command.")
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+        .setColor([135, 21, 153])
+        .setTimestamp();
+      message.channel.send({ embed })
+      return
+    };
+        const messageToSend = args.join(" ");
+        var guildList = client.guilds.array();
         try {
-            guildList.forEach(guild => guild.defaultChannel.send("messageToSend"));
+            guildList.forEach(guild => {
+               const upchannel = guild.channels.find("pg-updates");
+               if(!upchannel) return;
+               upchannel.send("<info:449011075569156097> Update: " + messageToSend)
+               
+         });
+        message.channel.send("<check:449010650019135519> Ran successfully")
         } catch (err) {
-            console.log("Could not send message to " + guild.name);
+            message.channel.send("<error:449010650010615828> Failed to run");
+            console.error(err);
         }
+}
