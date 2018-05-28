6be9e854ca18ad33528f0102f7a461193f171816
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-28 07:23:57 -0500

    Update eval.js

--- commands/eval.js
+++ commands/eval.js
@@ -5,8 +5,15 @@
     return text;
 };
 exports.run = (Discord, client, message, args) => {
-    if(message.author.id !== "424367587934339084") {
-      message.reply(`\`ERROR\` \`\`\`xl\nNice try, but no.\n\`\`\``);
+    if(message.author.id !== "424367587934339084" && message.author.id !== "388323805124362240") {
+      const embed = new  Discord.RichEmbed()
+        .setTitle("You shall NOT pass!")
+        .setDescription("You are not authorized to use this command.")
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+        .setColor([135, 21, 153])
+        .setTimestamp();
+      message.channel.send({ embed })
       return
     };
     try {
@@ -16,8 +23,24 @@ exports.run = (Discord, client, message, args) => {
       if (typeof evaled !== "string")
         evaled = require("util").inspect(evaled);
 
-      message.channel.send(clean(evaled), {code:"xl"});
+      const embed = new  Discord.RichEmbed()
+        .setTitle("Ran successfully")
+        .setDescription("The evaluation has ran successfully!")
+        .addField("Output", clean(evaled))
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+        .setColor([135, 21, 153])
+        .setTimestamp();
+      message.channel.send({embed})
     } catch (err) {
-      message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+      const embed = new  Discord.RichEmbed()
+        .setTitle("What has happened!?")
+        .setDescription("An error has occoured!")
+        .addField("Output", clean(err))
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+        .setColor([135, 21, 153])
+        .setTimestamp();
+      message.channel.send({embed})
     }
 }
