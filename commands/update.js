d5e7e08551f3634c826b23735a09bd0dda1ef383
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-28 14:25:26 -0500

    Update update.js

--- commands/update.js
+++ commands/update.js
@@ -10,15 +10,15 @@ exports.run = (Discord, client, message, args) => {
       message.channel.send({ embed })
       return
     };
+        function Ting(item) {
+                var upchannel = item.channels.find("pg-updates");
+               if(!upchannel) return;
+               upchannel.send("<:info:449011075569156097> Update: " + messageToSend);
+        }
         const messageToSend = args.join(" ");
         var guildList = client.guilds.array();
         try {
-            guildList.forEach(guild => {
-               const upchannel = guild.channels.find("pg-updates");
-               if(!upchannel) return;
-               upchannel.send("<:info:449011075569156097> Update: " + messageToSend);
-               
-         });
+            guildList.forEach(Ting);
         message.channel.send("<:check:449010650019135519> Ran successfully")
         } catch (err) {
             message.channel.send("<:error:449010650010615828> Failed to run");
