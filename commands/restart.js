65c6cb25fab103f707258d8397a81edcc93d1de0
ConJoined <39203143+ConJoined@users.noreply.github.com>
2018-05-28 19:14:30 -0500

    Create restart.js

---
+++ commands/restart.js
@@ -0,0 +1,23 @@
+exports.run = (Discord, client, message, args) => {
+   if(message.author.id !== "424367587934339084" && message.author.id !== "388323805124362240") {
+      const embed = new  Discord.RichEmbed()
+        .setTitle("You shall NOT pass!")
+        .setDescription("You are not authorized to use this command.")
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+        .setColor([135, 21, 153])
+        .setTimestamp();
+      message.channel.send({ embed })
+      return
+    };
+   const embed = new  Discord.RichEmbed()
+        .setTitle("Restarting bot...")
+        .setDescription("The bot will now restart. This might take a while.")
+        .setAuthor(message.author.username, message.author.displayAvatarURL)
+        .setFooter("Requested by " + message.author.tag, message.author.displayAvatarURL)
+        .setColor([135, 21, 153])
+        .setTimestamp();
+        message.reply({ embed } );
+        .then(msg => client.destroy())
+        .then(() => client.login(process.env.heart));
+}
